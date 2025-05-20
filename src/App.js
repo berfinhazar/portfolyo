805d3c97289a94da2d3241ec21b95e98d365c7ce
Berfin Hazar <berfinhazar05@gmail.com>
2025-05-21 01:21:21 +0300

    app.js güncellendi.

--- src/App.js
+++ src/App.js
@@ -14,27 +14,22 @@ function App() {
             <div className="app-container">
                 <Navbar />
                 <section id="anasayfa" style={{ paddingTop: '100px', minHeight: '100vh' }}>
-                    <h2>Ana Sayfa</h2>
                     <AnaSayfa />
-
                 </section>
 
                 <section id="benkimim" style={{ paddingTop: '100px', minHeight: '100vh' }}>
-                    <BenKimim /> {/* <-- EKLENDİ */}
+                    <BenKimim />
                 </section>
 
                 <section id="neler" style={{ paddingTop: '100px', minHeight: '100vh' }}>
-                    <h2>Neler Yapabilirim?</h2>
                     <NelerYapabilirim />
                 </section>
 
                 <section id="portfolyo" style={{ paddingTop: '100px', minHeight: '100vh' }}>
-                    <h2>Portfolyo</h2>
                     <Projelerim />
                 </section>
 
                 <section id="iletisim" style={{ paddingTop: '100px', minHeight: '100vh' }}>
-                    <h2>İletişim</h2>
                     <Iletisim />
                 </section>
             </div>
